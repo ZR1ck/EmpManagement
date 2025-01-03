ff163a26656f2c5f9e603b0f86fd21e12b765bb8
21120442 <duydangtr@gmail.com>
2025-01-03 21:58:29 +0700

    test api

--- Backend/EmpManagementAPI/src/main/java/com/example/EmpManagementAPI/controller/HomeController.java
+++ Backend/EmpManagementAPI/src/main/java/com/example/EmpManagementAPI/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.example.EmpManagementAPI.controller;
 
+import com.example.EmpManagementAPI.service.EmpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    private EmpService empService;
+
     @GetMapping("/test")
-    public String func() {
-        return "Hello World";
+    public ResponseEntity<?> func() {
+        try {
+            return new ResponseEntity<>(empService.getAllEmp(), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
