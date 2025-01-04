116cb6f42dda833474d016af260a9dd2c9553f14
21120442 <duydangtr@gmail.com>
2025-01-04 08:29:02 +0700

    fix auth err

--- Frontend/EmpManagementApp/src/layout/Login.jsx
+++ Frontend/EmpManagementApp/src/layout/Login.jsx
@@ -3,14 +3,14 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../contexts/AuthProvider';
 
+const host = process.env.REACT_APP_API_URL;
+
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
     const navigate = useNavigate();
     const { login } = useAuthContext();
-
-    const host = process.env.REACT_APP_API_URL;
 
     const handleSubmit = async (e) => {
         e.preventDefault();
--- Frontend/EmpManagementApp/src/layout/Register.jsx
+++ Frontend/EmpManagementApp/src/layout/Register.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+const host = process.env.REACT_APP_API_URL;
+
 function Login() {
     const [empid, setEmpid] = useState('');
     const [username, setUsername] = useState('');
@@ -9,7 +11,6 @@ function Login() {
     const [error, setError] = useState(null);
     const navigate = useNavigate();
 
-    const host = process.env.REACT_APP_API_URL;
 
     const handleSubmit = async (e) => {
         e.preventDefault();
